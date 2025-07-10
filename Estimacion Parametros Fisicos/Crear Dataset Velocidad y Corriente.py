b7e2788c570e8ddea3c14bd6920c270582f2a643
Alan-Barrera-Preciado <alan.barrerapreciado@gmail.com>
2025-07-10 12:45:47 -0600

    Creacion de Dataset para 2 motores simultaneamente

--- Estimacion Parametros Fisicos/Crear Dataset Velocidad y Corriente.py
+++ Estimacion Parametros Fisicos/Crear Dataset Velocidad y Corriente.py
@@ -9,10 +9,20 @@ time.sleep(2)
 intervalo_comunicacion=0.05
 tiempo_actual=0
 
-vector_rpm=[]
+vector_radianes_Izq=[]
+vector_corriente_Izq=[]
+
+vector_radianes_Der=[]
+vector_corriente_Der=[]
+
 vector_tiempo=[]
 
-vector_rpm.append(0)
+vector_radianes_Izq.append(0)
+vector_corriente_Izq.append(0)
+
+vector_radianes_Der.append(0)
+vector_corriente_Der.append(0)
+
 vector_tiempo.append(0)
 
 N = 400
@@ -21,8 +31,15 @@ for k in range(N):
     cad = f"{1}\n"
     serialArduino.write(cad.encode('ascii'))
     Lectura = serialArduino.readline().decode('ascii').rstrip('\n')
-    RPM, Control = map(float, Lectura.split(','))
-    vector_rpm.append(RPM)
+    Motor_Izq, Motor_Der = Lectura.split(':')
+    Rad_Izq, Corriente_Izq = map(float, Motor_Izq.split(','))
+    Rad_Der, Corriente_Der = map(float, Motor_Der.split(','))
+    Corriente_Izq = Corriente_Izq/1000
+    Corriente_Der = Corriente_Der/1000
+    vector_radianes_Izq.append(Rad_Izq)
+    vector_corriente_Izq.append(Corriente_Izq)
+    vector_radianes_Der.append(Rad_Der)
+    vector_corriente_Der.append(Corriente_Der)
     tiempo_actual=tiempo_actual+intervalo_comunicacion
     vector_tiempo.append(tiempo_actual)
     
@@ -33,7 +50,12 @@ cad = f"{0}\n"
 serialArduino.write(cad.encode('ascii'))
 serialArduino.close()
 
-data = {'t':vector_tiempo,'Radianes':vector_rpm}
+data_Izq = {'t':vector_tiempo,'Radianes':vector_radianes_Izq, 'Corriente':vector_corriente_Izq}
+data_Der = {'t':vector_tiempo,'Radianes':vector_radianes_Der, 'Corriente':vector_corriente_Der}
 
-df = pd.DataFrame(data)
-df.to_csv('df_example_12v.csv',index=False)
+
+df_Izq = pd.DataFrame(data_Izq)
+df_Izq.to_csv('df_12v.csv',index=False)
+
+df_Der = pd.DataFrame(data_Der)
+df_Der.to_csv('df_24v.csv',index=False)
--- Estimacion Parametros Fisicos/LecturasVelocidadCorriente/LecturasVelocidadCorriente.ino
+++ Estimacion Parametros Fisicos/LecturasVelocidadCorriente/LecturasVelocidadCorriente.ino
@@ -1,24 +1,55 @@
-int MotorSH = 6;
-int MotorSA = 5;
+#include <Wire.h>
+#include <INA226_WE.h>
+INA226_WE ina226_Izq = INA226_WE(0x40);  // Dirección I2C por defecto
+INA226_WE ina226_Der = INA226_WE(0x41);  // Dirección I2C por defecto
 
-int Resolucion = 800;
-int Encoder = 2;
-volatile byte Pulsos = 0;
-int PulsosIntervalo = 0;
+int MotorSH_Izq = 6;
+int MotorSA_Izq = 5;
 
-float RPM = 0;
-int Aux = 1;
-float Rad = 0;
-float t = 0;
+int MotorSH_Der = 9;
+int MotorSA_Der = 10;
+
+int Encoder_Izq = 2;
+int Encoder_Der = 3;
+int Resolucion_Izq = 800;
+int Resolucion_Der = 700;
+volatile byte Pulsos_Izq = 0;
+volatile byte Pulsos_Der = 0;
+int PulsosIntervalo_Izq = 0;
+int PulsosIntervalo_Der = 0
+
+float Corriente_Izq = 0;
+float Corriente_Der = 0;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(MotorSH, OUTPUT);
-  pinMode(MotorSA, OUTPUT);
-  analogWrite(MotorSH, 0);
-  analogWrite(MotorSA, 0);
-  pinMode(Encoder, INPUT);
-  attachInterrupt(digitalPinToInterrupt(Encoder), Contador, RISING);
+  Wire.begin();
+  pinMode(MotorSH_Izq, OUTPUT);
+  pinMode(MotorSA_Izq, OUTPUT);
+  analogWrite(MotorSH_Izq, 0);
+  analogWrite(MotorSA_Izq, 0);
+
+  pinMode(MotorSH_Der, OUTPUT);
+  pinMode(MotorSA_Der, OUTPUT);
+  analogWrite(MotorSH_Der, 0);
+  analogWrite(MotorSA_Der, 0);
+
+  pinMode(Encoder_Izq, INPUT);
+  attachInterrupt(digitalPinToInterrupt(Encoder_Izq), Contador_Izq, RISING);
+  pinMode(Encoder_Der, INPUT);
+  attachInterrupt(digitalPinToInterrupt(Encoder_Der), Contador_Der, RISING);
+
+  if (!ina226_Izq.init()) {
+    Serial.println("INA226 Izq no detectado.");
+  }
+  if (!ina226_Der.init()) {
+    Serial.println("INA226 Der no detectado.");
+  }
+
+  ina226_Izq.setAverage(AVERAGE_16);
+  ina226_Izq.setConversionTime(CONV_TIME_1100);
+  ina226_Der.setAverage(AVERAGE_16);
+  ina226_Der.setConversionTime(CONV_TIME_1100);
 }
 
 void loop() {
@@ -26,55 +57,90 @@ void loop() {
     while(Aux == 1){
       delay(50);
       if(t < 2){
-        analogWrite(MotorSH, 0);
+        analogWrite(MotorSH_Izq, 0);
+        analogWrite(MotorSH_Der, 0);
       }
       else if(t < 4 && t >= 2){
-        analogWrite(MotorSH, 255);
+        analogWrite(MotorSH_Izq, 255);
+        analogWrite(MotorSH_Der, 255);
       }
       else if(t < 6 && t >= 4){
-        analogWrite(MotorSH, 0);
+        analogWrite(MotorSH_Izq, 0);
+        analogWrite(MotorSH_Der, 0);
       }
       else if(t < 8 && t >= 6){
-        analogWrite(MotorSH, 200);
+        analogWrite(MotorSH_Izq, 200);
+        analogWrite(MotorSH_Der, 200);
       }
       else if(t < 10 && t >= 8){
-        analogWrite(MotorSH, 0);
+        analogWrite(MotorSH_Izq, 0);
+        analogWrite(MotorSH_Der, 0);
       }
       else if(t < 12 && t >= 10){
-        analogWrite(MotorSH, 150);
+        analogWrite(MotorSH_Izq, 150);
+        analogWrite(MotorSH_Der, 150);
       }
       else if(t < 14 && t >= 12){
-        analogWrite(MotorSH, 0);
+        analogWrite(MotorSH_Izq, 0);
+        analogWrite(MotorSH_Der, 0);
       }
       else if(t < 16 && t >= 14){
-        analogWrite(MotorSH, 100);
+        analogWrite(MotorSH_Izq, 100);
+        analogWrite(MotorSH_Der, 100);
       }
       else if(t < 18 && t >= 16){
-        analogWrite(MotorSH, 0);
+        analogWrite(MotorSH_Izq, 0);
+        analogWrite(MotorSH_Der, 0);
       }
       else if(t < 20){
-        analogWrite(MotorSH, 50);
+        analogWrite(MotorSH_Izq, 50);
+        analogWrite(MotorSH_Der, 50);
       }
-      RPM = CalcularRPM();
-      Rad = RPM*0.10472;
+      EnviarDatos();
       String cad = Serial.readStringUntil('\n');
-      Serial.println(Rad);
       Aux = cad.toInt();
       t = t + 0.05;
     }
-    analogWrite(MotorSH, 0);
+    analogWrite(MotorSH_Izq, 0);
+    analogWrite(MotorSH_Der, 0);
   }
 }
 
-void Contador(){
-  Pulsos++;
+void Contador_Izq(){
+  Pulsos_Izq++;
+}
+void Contador_Der(){
+  Pulsos_Der++;
 }
 
-float CalcularRPM(){
+float CalcularRPM_Izq(){
   noInterrupts();
-  PulsosIntervalo = Pulsos;
-  Pulsos = 0;
-  RPM = (PulsosIntervalo/0.05)*60/Resolucion;
+  PulsosIntervalo_Izq = Pulsos_Izq;
+  Pulsos_Izq = 0;
+  RPM_Izq = (PulsosIntervalo_Izq/0.05)*60/Resolucion_Izq;
   interrupts();
-  return RPM;
+  return RPM_Izq;
+}
+
+float CalcularRPM_Der(){
+  noInterrupts();
+  PulsosIntervalo_Der = Pulsos_Der;
+  Pulsos_Der = 0;
+  RPM_Der = (PulsosIntervalo_Der/0.05)*60/Resolucion_Der;
+  interrupts();
+  return RPM_Der;
+}
+
+void EnviarDatos() {
+  RPM_Izq = CalcularRPM_Izq();
+  RPM_Der = CalcularRPM_Der();
+  Corriente_Izq = ina226_Izq.getCurrent_mA();
+  Corriente_Der = ina226_Der.getCurrent_mA();
+  Serial.print(RPM_Izq);
+  Serial.print(",");
+  Serial.print(Corriente_Izq);
+  Serial.print(":");
+  Serial.print(RPM_Der);
+  Serial.print(",");
+  Serial.println(Corriente_Der);
 }
