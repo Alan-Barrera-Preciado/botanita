024f21c235e2675a32beeba18c4be66a2790eb37
ALan-Barrera-Preciado <alan.barrerapreciado@gmail.com>
2025-08-01 12:49:34 -0600

    Creacion workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/modularpc/Desktop/Proyecto-Modular-Botanita/catkin_ws/src
+# Build directory: /home/modularpc/Desktop/Proyecto-Modular-Botanita/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("Control_Motores")
+subdirs("control_motores")
---
+++ catkin_ws/build/Control_Motores/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/modularpc/Desktop/Proyecto-Modular-Botanita/catkin_ws/src/Control_Motores
+# Build directory: /home/modularpc/Desktop/Proyecto-Modular-Botanita/catkin_ws/build/Control_Motores
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
