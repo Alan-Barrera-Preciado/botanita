f5710889215772b699af3c9663693a36cafa5e81
Alan-Barrera-Preciado <alan.barrerapreciado@gmail.com>
2025-08-25 14:02:36 -0600

    Delete catkin_ws directory
    Se borro el Workspace ya que se agrego erróneamente al repositorio, se actualiza a una estructura mejor

--- catkin_ws/build/control_motores/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/botanita/Desktop/Proyecto-Modular-Botanita/catkin_ws/src/control_motores
-# Build directory: /home/botanita/Desktop/Proyecto-Modular-Botanita/catkin_ws/build/control_motores
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
