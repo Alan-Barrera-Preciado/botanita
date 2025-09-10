794651e9899d3b72e030cfe74b37b9f4918cacbb
artificial-riot <ferchero055@hotmail.com>
2025-09-10 13:00:50 -0600

    se añaden nuevos codigos ESP y para graficar .csv

---
+++ src/ESP32-Codigos/Motores/funciones.cpp
@@ -0,0 +1,124 @@
+#include "funciones.h"
+// Funciones //
+
+// INA226 //
+void ina226Setup(INA226_WE &ina226, String name){
+  if(!ina226.init()){
+    Serial.println("INA226 no encontrado en " + name);
+    while(1);
+  }
+  ina226.setAverage(AVERAGE_64);
+  ina226.setConversionTime(CONV_TIME_332);
+  ina226.waitUntilConversionCompleted();
+
+  Serial.println("INA226 " + name + " configurado.");
+}
+
+float ina226Read(INA226_WE &ina226, String name){
+  float current = ina226.getCurrent_mA();
+  
+  if (!isfinite(current)) {
+    current = 0.0;    // o enviar "nan" deliberado y manejar en Python
+  }
+  
+  String outstr = String(current, 3);
+
+  return current; // outstr current
+}
+
+// MOTORES //
+
+void setupEncoder(int pinInterrupcion, String RL){
+  pinMode(pinInterrupcion, INPUT_PULLUP);
+  if(RL == "R"){
+    attachInterrupt(digitalPinToInterrupt(pinInterrupcion), incrementarR, RISING);  
+  } else if (RL == "L") {
+    attachInterrupt(digitalPinToInterrupt(pinInterrupcion), incrementarL, RISING);  
+  }
+  
+}
+
+
+void IRAM_ATTR incrementarR(){contadorR++;} // para encoder 
+void IRAM_ATTR incrementarL(){contadorL++;}
+
+void pwmSetup(int pinAdelante, int pinAtras){
+  // Configuración PWM con API nueva (core 3.x)
+  ledcAttach(pinAdelante, 5000, 8);   // pin, frecuencia, resolución
+  ledcWrite(pinAdelante, 0);          // duty inicial
+  
+  ledcAttach(pinAtras, 5000, 8);   // pin, frecuencia, resolución
+  ledcWrite(pinAtras, 0);          // duty inicial
+  
+  //pinMode(pinAdelante, OUTPUT);
+  //analogWrite(pinAdelante, 0);
+  //pinMode(pinAtras, OUTPUT);
+  //analogWrite(pinAtras, 0);
+}
+
+float calcularRPM(volatile unsigned long &contadorPulsos, int res, float dt){
+  noInterrupts();
+
+  int intervaloPulsos = contadorPulsos;
+  contadorPulsos = 0;
+  interrupts();
+
+  float rpm = (intervaloPulsos/dt*60/res);
+  String outstr = String(rpm, 3);
+  // Serial.println(outstr);
+
+  return rpm; // outstr rpm
+}
+
+void changePWM(int pinAdelante, int pinAtras, int trabajo){
+  if(trabajo >= 0){
+    ledcWrite(pinAtras, 0);
+    ledcWrite(pinAdelante, trabajo);
+    
+    //analogWrite(pinAtras, 0);
+    //analogWrite(pinAdelante, trabajo);  
+  } else {
+    ledcWrite(pinAdelante, 0);
+    ledcWrite(pinAtras, trabajo*-1);
+    
+    //analogWrite(pinAdelante, 0);
+    //analogWrite(pinAtras, trabajo*-1); 
+  }
+}
+
+// COMUNICACION //
+
+String getFromPy(){
+  // Esperar índice desde Python y responderlo
+  while (!Serial.available());
+  String idxStr = Serial.readStringUntil('\n');
+  return idxStr;
+  // Serial.println(idxStr); // reenviarlo para validación en Python
+}
+
+void serialToArray(int *arrayDestino) {
+  String cad = Serial.readStringUntil('\n');
+  cad.trim();
+
+  if (cad.length() > 0) {
+    int sep = cad.indexOf(',');
+    if (sep > 0) {
+      ArrayControl[0] = cad.substring(0, sep).toInt();
+      ArrayControl[1] = cad.substring(sep + 1).toInt();
+    }
+  }
+
+  // Serial.println("Cadena original: [" + cad + "]");
+
+  //int pwmR = 0, pwmL = 0;
+  //sscanf(cad.c_str(), "%*[^0-9-]%d,%d", &pwmR, &pwmL);  // limpia basura
+
+  //arrayDestino[0] = pwmR;
+  //arrayDestino[1] = pwmL;
+}
+
+void control(){
+  serialToArray(ArrayControl);
+  changePWM(pwmRAdelante, pwmRAtras, ArrayControl[0]);  // R
+  changePWM(pwmLAdelante, pwmLAtras, ArrayControl[1]);  // L
+}
