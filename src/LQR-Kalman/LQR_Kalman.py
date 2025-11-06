a7cb72a4ef17d1707a49058c7a5a2c771215ec1d
Alan-Barrera-Preciado <alan.barrerapreciado@gmail.com>
2025-11-06 13:41:19 -0600

    Pruebas finales 10

--- src/LQR-Kalman/LQR_Kalman.py
+++ src/LQR-Kalman/LQR_Kalman.py
@@ -26,8 +26,8 @@ dataset_dir = os.path.join(pkg_path, "src/LQR-Kalman/datasets")
 # Corriente, Velocidad
 # Q, R
 
-Kalman_Izq = [[1e-8, 5e-14], [3e-10, 5e-11]]
-Kalman_Der = [[1e-8, 5e-14], [3e-10, 5e-11]]
+Kalman_Izq = [[1e-9, 5e-14], [3e-10, 5e-11]]
+Kalman_Der = [[1e-9, 5e-14], [3e-10, 5e-11]]
 
 # Corriente, Velocidad, Accion de control
 
