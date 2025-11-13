2079deaf73d4df6678f069058749d8279bbcdb92
Alan <dxalan@gmail.com>
2025-11-12 20:27:01 -0600

    Comentarios codigo de odometria

--- src/Control-Botanita/Odometria.py
+++ src/Control-Botanita/Odometria.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
 
+# -------- Librerias ----------
+
 import rospy
 from std_msgs.msg import Float32MultiArray, Float32
 from nav_msgs.msg import Odometry
 import tf
 import math
 
+# -------- Nodo de ROS y publicadores ----------
+
 rospy.init_node('odometria')
 pub = rospy.Publisher('/odom_estimada', Odometry, queue_size=10)
 theta_pub = rospy.Publisher('/theta_estimada', Float32, queue_size=10)   # orientación en grados
 
-r = 0.08    # radio de las ruedas [m]
-L = 0.405   # distancia entre ruedas [m]
+# -------- Parametros iniciales ----------
+
+r = 0.08    # Radio de las ruedas [m]
+L = 0.405   # Distancia entre ruedas [m]
 w_l = 0.0
 w_r = 0.0
 
 x, y, theta = 0.0, 0.0, 0.0
 prev_time = None
-v_f, omega_f = 0.0, 0.0   # inicialización de filtros
+v_f, omega_f = 0.0, 0.0
+
+# -------- Recibir mensaje de ROS del topic al que se esta suscrito ----------
 
 def rads_callback(msg):
     global x, y, theta, prev_time, v_f, omega_f
@@ -29,21 +37,24 @@ def rads_callback(msg):
     omega = r * (w_r - w_l) / L
 
     current_time = rospy.Time.now()
-    dt = 0.01 # if prev_time is None else (current_time - prev_time).to_sec()
+    dt = 0.01
     prev_time = current_time
 
-    # Filtro exponencial
+# -------- Filtro exponencial ----------
+
     alpha = 0.4
     v_f = alpha*v + (1-alpha)*v_f
     omega_f = alpha*omega + (1-alpha)*omega_f
 
-    # Integración
+# -------- Integracion ----------
+
     theta_mid = theta + 0.5*omega_f*dt
     x += v_f * math.cos(theta_mid) * dt
     y += v_f * math.sin(theta_mid) * dt
     theta += omega_f * dt
 
-    # --- Mensaje Odometry ---
+# -------- Construccion de mensaje de odometria ----------
+
     odom_msg = Odometry()
     odom_msg.header.stamp = current_time
     odom_msg.header.frame_id = "odom"
@@ -62,9 +73,12 @@ def rads_callback(msg):
 
     pub.publish(odom_msg)
 
-    # --- Publicar theta en grados ---
+# -------- Publicar Theta en grados en topic diferente ----------
+
     theta_deg = math.degrees(theta)
     theta_pub.publish(theta_deg)
+
+# -------- Suscripcion de ROS ----------
 
 rospy.Subscriber('/rads_motores', Float32MultiArray, rads_callback)
 rospy.spin()
